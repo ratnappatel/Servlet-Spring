0fa3aca34e509d0255d1164c5504daf07b4dae4e
ratnappatel <ratnappatel@gmail.com>
2022-02-09 11:17:28 +0530

    Spring AOP Basic Example

---
+++ SpringExamples/src/main/java/com/gl/aop/HijackThrowException.java
@@ -0,0 +1,9 @@
+package com.gl.aop;
+
+import org.springframework.aop.ThrowsAdvice;
+
+public class HijackThrowException implements ThrowsAdvice {
+	public void afterThrowing(IllegalArgumentException e) throws Throwable {
+		System.out.println("HijackThrowException : Throw exception hijacked!");
+	}
+}
